4c4de0f3756c369c3bbd192f244305830211fcf8
Aleks-corp <101347633+Aleks-corp@users.noreply.github.com>
2023-09-13 17:07:36 +0000

    Deploying to gh-pages from @ Aleks-corp/car-rent-test-job@d93eb439bb1d129502ddda37db1ca064d377630d 🚀

---
+++ assets/CatalogPage-cf7fa1b3.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-7098706e.js";const e=()=>a.jsx("div",{children:"CatalogPage"});export{e as default};
---
+++ assets/MainPage-0bc3cef6.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-7098706e.js";const e=()=>r.jsx("div",{children:"MAINPAGE"});export{e as default};
