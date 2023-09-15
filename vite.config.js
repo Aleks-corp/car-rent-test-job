785812a5429de9f0bfe1b57855c47fce3eeae31b
Aleks Haran <nporock@gmail.com>
2023-09-15 19:12:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/car-rent-test-job",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve("index.html"),
+        404: resolve("404.html"),
+      },
+    },
+  },
 });
